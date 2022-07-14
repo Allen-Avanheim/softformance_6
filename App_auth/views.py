144e8dee59c9a87d82e8b08b2d768143365b0789
Allen-Avanheim <86322722+Allen-Avanheim@users.noreply.github.com>
2022-07-14 20:55:47 +0300

    Initial commit

---
+++ App_auth/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, HttpResponseRedirect, reverse
+
+
+def home(request):
+    return render(request, 'App_auth/home.html')
+
+
+def login_or_signup(request):
+    return render(request, 'App_auth/login_or_signup.html')
+
+
+def login_signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(username=email, password=password)
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('App_auth:home'))
+        elif not User.objects.filter(username=email):
+            create_user = User(username=email)
+            create_user.set_password(password)
+            create_user.save()
+            user = authenticate(username=email, password=password)
+            login(request, user)
+            return HttpResponseRedirect(reverse('App_auth:home'))
+        else:
+            return HttpResponseRedirect(reverse('App_auth:login-or-signup'))
+
+    return HttpResponseRedirect(reverse('App_auth:login-signup'))
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('App_auth:login-or-signup'))
---
+++ README.md
@@ -0,0 +1,12 @@
+Django Task #3 (REST API)
+
+Build 2 Django REST API backend endpoints that will log in the user with 2 different methods:
+
+●Regular login with email and password
+●Login via FB Connect or Google OAuth method. Please, if possible avoid ready-to-use Django addons for 3rd party logins.
+
+We don’t need any UI, forms, and login pages.
+
+Just end points that will double-check received user data, check with the database or external resource if it’s valid, and open user session. And finally, give back auth token.
+
+You can use the default Django User model for this task.
